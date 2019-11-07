a6bb76bdc8561bb311cc1987b677a4e7cbcbb982
Jianshuo Qiu <Platinum@Jianshuos-MacBook-Pro.local>
2019-11-06 22:43:53 -0500

    answered search_insert_position

---
+++ divide_and_conquer/search_insert_position.py
@@ -0,0 +1,29 @@
+# Runtime: 48 ms, faster than 99.57% of Python3 online submissions for Search Insert Position.
+# Memory Usage: 13.6 MB, less than 83.58% of Python3 online submissions for Search Insert Position.
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        def binarySearch(nums, target, start, end):
+            print(start)
+            print(end)
+            print(' ')
+            
+            mid = (start + end)//2
+            if nums[mid] == target:
+                return mid
+            
+            if start >= end:
+                
+                if target > nums[start]:
+                    return start + 1
+                else:
+                    return start
+            
+            if target > nums[mid]:
+                return binarySearch(nums, target, mid+1, end)
+            else:
+                return binarySearch(nums, target, start, mid-1)
+        
+        if not nums:
+                return 0
+        return binarySearch(nums, target, 0, len(nums)-1)
